7945e93ff897bf5e5cc9a257f0fdcd21177b65f8
emreakay <emre.akay@aurorabilisim.com>
2023-11-25 13:30:55 +0300

    some fix

--- config/connective.php
+++ config/connective.php
@@ -2,8 +2,10 @@
 
 // config for AuroraWebSoftware/Connective
 return [
+    /*
     'connection_types' => [
         ['a' => ['relationship' => 'one']],
         ['b' => ['relationship' => 'many']],
     ],
+    */
 ];
